ebda0214f91231ba73a58b3c485a40295717048f
Renzil D'Souza <renzil@gmail.com>
2022-11-30 10:29:21 +0000

    feat: google sign in

--- web-app/src/routes/+page.svelte
+++ web-app/src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<svelte:head>
+   <script src="https://accounts.google.com/gsi/client" async defer></script>
+</svelte:head>
+
 <h1 class="text-3xl font-bold">Bank Statement Parser</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="g_id_onload"
+   data-client_id="419266631077-7e3r3k96neg1upb7vv1kjoj5cqqr1ei0.apps.googleusercontent.com"
+   data-login_uri="https://renzil-effective-guide-pjrrp9jqx43rp7v-5173.preview.app.github.dev"
+   data-auto_prompt="false">
+</div>
+<div class="g_id_signin"
+   data-type="standard"
+   data-size="large"
+   data-theme="outline"
+   data-text="sign_in_with"
+   data-shape="rectangular"
+   data-logo_alignment="left">
+</div>
