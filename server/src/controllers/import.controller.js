6ed660f77162169d849b003d4b1316494250a406
Renzil D'Souza <renzil@gmail.com>
2023-01-03 04:30:24 +0000

    feat: upload file name

--- server/src/controllers/import.controller.js
+++ server/src/controllers/import.controller.js
@@ -3,7 +3,7 @@ const catchAsync = require('../utils/catchAsync');
 const { importService } = require('../services');
 
 const uploadRequest = catchAsync(async (req, res) => {
-    const result = await importService.uploadRequest(req.body);
+    const result = await importService.uploadRequest(req.user._id.toString());
     res.status(httpStatus.CREATED).send(result);
 });
 
